2945afc88274186a002625fe9ec5bd615fdc4671
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-04-12 17:11:52 -0600

    messin around with navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -29,11 +29,11 @@ const Navbar = () => {
   const shouldShowNavbar = isSpecialRoute || showNavbar;
 
   // Add the 'fixed' class if the navbar should be fixed to the top of the screen
-  const navbarClass = `navbar bg-red-400 ${shouldShowNavbar ? 'show' : 'hide'} ${isSpecialRoute ? 'fixed' : ''}`;
+  const navbarClass = `navbar ${shouldShowNavbar ? 'show' : 'hide'} ${isSpecialRoute ? 'fixed' : ''}`;
 
   return (
     <nav className={navbarClass}>
-      <ul className="flex flex-row justify-between align-between">
+      <ul className="flex flex-row justify-around">
         <li className="p-4"><a href="/">Home</a></li>
         <li className="p-4"><a href="/">About</a></li>
         <li className="p-4"><a href="/">Work</a></li>
@@ -43,3 +43,6 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+
