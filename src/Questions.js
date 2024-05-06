b2c69a37fca1e486aee5af068f33fc3841af2a1a
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-05-06 10:34:34 -0600

    updated Qs

--- src/Questions.js
+++ src/Questions.js
@@ -6,7 +6,6 @@ function QandA() {
             <h2 className="subHeading">Q & A</h2>
             <ul className="space-y-8">
                 <li>What is your favorite aspect of software engineering?</li>
-                <li>My favorite aspect of software engineering is the structured problem-solving inherent when programming. There's a satisfaction in dissecting complex issues and crafting elegant solutions through code.</li>
                 <li>What are some goals or aspirtations you have for your career in software development?</li>
                 <li>What motivates you to continue to learn and grow as a software engineer?</li>
             </ul>
