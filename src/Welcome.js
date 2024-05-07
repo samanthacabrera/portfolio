5cf947b516c0db793d1a1d852b007a53f016510e
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-07 20:12:05 +0000

    Adjust Welcome component to improve mobile responsiveness

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 function Welcome() {
-    return (
-        <div className="text-center space-y-80 mx-48">
-            <p>Welcome to my personal website, <br/>an amalgam of everything that makes me... <br/>well me. </p>
-            <p>As you scroll, you'll uncover demos, blog posts, and project walkthroughs all centered around my work as a full-stack web developer.</p>
-            <p>(Please Scroll Responsibly)</p>
-        </div>
-    )
+  return (
+    <div className="text-center space-y-80 md:mx-48 mx-2">
+      <p>Welcome to my personal website, <br/>an amalgam of everything that makes me... <br/>well me. </p>
+      <p>As you scroll, you'll uncover demos, blog posts, and project walkthroughs all centered around my work as a
+        full-stack web developer.</p>
+      <p>(Please Scroll Responsibly)</p>
+    </div>
+  )
 }
 
 export default Welcome;
