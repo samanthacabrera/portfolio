06cdc6839dcd0c0225b72b9656ecad6b552797ae
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-07 20:12:03 +0000

    Increase font size and add spacing for Q&A section

--- src/Questions.js
+++ src/Questions.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 function QandA() {
-    return (
-       <div className="space-y-20">
-            <h2 className="subHeading">Q & A</h2>
-            <ul className="space-y-8">
-                <li>What is your favorite aspect of software engineering?</li>
-                <li>What are some goals or aspirtations you have for your career in software development?</li>
-                <li>What motivates you to continue to learn and grow as a software engineer?</li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="space-y-20">
+      <h2 className="subHeading pt-24 md:pb-12  md:text-9xl text-7xl">Q & A</h2>
+      <ul className="space-y-8">
+        <li>What is your favorite aspect of software engineering?</li>
+        <li>What are some goals or aspirtations you have for your career in software development?</li>
+        <li>What motivates you to continue to learn and grow as a software engineer?</li>
+      </ul>
+    </div>
+  )
 }
 
 export default QandA
