eafeb70f03bfe7af6d6211a8f3e1f23d4d71a076
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-07 20:11:59 +0000

    Enhance Inspo section with responsive styles and content updates

--- src/Inspo.js
+++ src/Inspo.js
@@ -1,27 +1,28 @@
 import React from "react";
 
 function Inspo() {
-    return (
-        <div className="space-y-20">
-            <h2 className="subHeading">Inspo</h2>
-            <p>I find inspiration through music, art, and cinema. I am drawn to abstract and thought-provoking themes. Below are some of my regular sources of inspiration:</p>   
+  return (
+    <div className="space-y-20">
+      <h2 className="subHeading pt-24 md:pb-12  md:text-9xl text-7xl">Inspo</h2>
+      <p>I find inspiration through music, art, and cinema. I am drawn to abstract and thought-provoking themes. Below
+        are some of my regular sources of inspiration:</p>
 
-            <ul className="space-y-2">
-                <li>Jean-Michel Basquiat</li>
-                <li>Frida Kahlo</li>
-                <li>Damien Hirst</li>
-                <li>Steven J. Yazzie</li>
-                 <li>Tadao Ando</li>
-                <li>Hayao Miyazaki</li>
-                <li>@hozzography</li>
-                <li>Tarsila do Amaral</li>
-                <li>Francisco Goya</li>
-                <li>Emily Bronte</li>
-                <li>Gustav Klimt</li>
-                <li>Patricia Riggen</li>
-            </ul>    
-        </div>
-    )
+      <ul className="space-y-2">
+        <li>Jean-Michel Basquiat</li>
+        <li>Frida Kahlo</li>
+        <li>Damien Hirst</li>
+        <li>Steven J. Yazzie</li>
+        <li>Tadao Ando</li>
+        <li>Hayao Miyazaki</li>
+        <li>@hozzography</li>
+        <li>Tarsila do Amaral</li>
+        <li>Francisco Goya</li>
+        <li>Emily Bronte</li>
+        <li>Gustav Klimt</li>
+        <li>Patricia Riggen</li>
+      </ul>
+    </div>
+  )
 }
 
 export default Inspo;
