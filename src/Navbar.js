2edaa8e403d9558d296b0729bdd49fd1b33cda7b
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-07 20:12:01 +0000

    Adjust navbar layout and spacing for mobile devices

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub, faCodepen, faSpotify } from '@fortawesome/free-brands-svg-icons';
+import React, {useState, useEffect} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faLinkedin, faGithub, faCodepen, faSpotify} from '@fortawesome/free-brands-svg-icons';
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
@@ -29,17 +29,21 @@ const Navbar = () => {
     <nav className={navbarClass}>
       <div className="flex flex-row items-center justify-between px-4">
         <a id="homeIcon" href="/">Sam<span> Cab</span></a>
-        <div className="flex flex-row items-center space-x-20 py-2">
-          <a href="https://linkedin.com/in/samcabreraa" target="_blank" rel="noopener noreferrer" className="icon-link hover:-translate-y-1 transition duration-300 ease-in-out">
+        <div className="flex flex-row items-center md:space-x-20 space-x-5 py-2">
+          <a href="https://linkedin.com/in/samcabreraa" target="_blank" rel="noopener noreferrer"
+             className="icon-link hover:-translate-y-1 transition duration-300 ease-in-out">
             <FontAwesomeIcon icon={faLinkedin} size="2x"/>
           </a>
-          <a href="https://github.com/samanthacabrera" target="_blank" rel="noopener noreferrer" className="icon-link hover:-translate-y-1 transition duration-300 ease-in-out">
+          <a href="https://github.com/samanthacabrera" target="_blank" rel="noopener noreferrer"
+             className="icon-link hover:-translate-y-1 transition duration-300 ease-in-out">
             <FontAwesomeIcon icon={faGithub} size="2x"/>
           </a>
-          <a href="https://codepen.io/samanthacabrera" target="_blank" rel="noopener noreferrer" className="icon-link hover:-translate-y-1 transition duration-300 ease-in-out">
+          <a href="https://codepen.io/samanthacabrera" target="_blank" rel="noopener noreferrer"
+             className="icon-link hover:-translate-y-1 transition duration-300 ease-in-out">
             <FontAwesomeIcon icon={faCodepen} size="2x"/>
           </a>
-          <a href="https://open.spotify.com/user/samantha.n.cabrera?si=mAb874ggRgu9g60cUOlMPw" target="_blank" rel="noopener noreferrer" className="icon-link hover:-translate-y-1 transition duration-300 ease-in-out">
+          <a href="https://open.spotify.com/user/samantha.n.cabrera?si=mAb874ggRgu9g60cUOlMPw" target="_blank"
+             rel="noopener noreferrer" className="icon-link hover:-translate-y-1 transition duration-300 ease-in-out">
             <FontAwesomeIcon icon={faSpotify} size="2x"/>
           </a>
         </div>
