6063237769f992958d915d33e285846fe7bd3eb6
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-05-12 12:37:49 -0600

    added progress bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProgressBar from './Progress';
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Welcome from './Welcome';
@@ -16,9 +17,17 @@ import './index.css';
 
 
 function App() {
+  document.addEventListener("DOMContentLoaded", function () {
+    document.body.addEventListener("keydown", function (event) {
+      let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+      document.body.style.backgroundColor = randomColor;
+    });
+  });
+
   return (
     <Router>
       <section className="parallax">
+        <ProgressBar />
         <Navbar />
         <Switch>
           <Route exact path="/">
--- src/Bio.js
+++ src/Bio.js
@@ -3,14 +3,12 @@ import me from "./IMG_0897.JPG";
 
 function Bio() {
   return (
-    <div className="space-y-10 md:m-40 m-12">
+    <div className="space-y-20 md:m-40 m-12">
       <h2 className="subHeading lowercase md:text-9xl text-7xl">All <br /> About Me </h2>
       <img src={me} alt="Me" />
       <p>I was born and raised in Southern California. I currently live in Denver with my husband, Justin, and our cute
-        cat, Ares.</p>
-      <p>In my free time you can find me practing yoga, solving puzzles, or watching movies. When the weather permits, I
-        love to go on picnics and hike.</p>
-      <p>I am in constant pursuit of new experiences, embracing the richness that life has to offer.</p>
+        cat, Ares. In my free time you can find me practing yoga, solving puzzles, or watching movies. When the weather permits, I
+        love to go on picnics and hike. I am in constant pursuit of new experiences, embracing the richness that life has to offer.</p>
     </div>
   )
 }
