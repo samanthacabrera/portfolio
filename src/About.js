1f8343b41e1c6f3f94cd3c0e3b5e23cabf3093a2
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-06-12 19:58:04 -0600

    jdsf

--- src/About.js
+++ src/About.js
@@ -19,7 +19,7 @@ function About() {
         <h2 className="subHeading lowercase md:text-9xl text-7xl">Q & A</h2>
 
         <p>What is your favorite aspect of software engineering?</p>
-        <p>I love being able to create any idea that pops into my head. I have always felt like I had so many ideas, but no way to feasibly turn them into a reality.</p>
+        <p>I love being able to create any idea that pops into my head into a web application.</p>
         <p>What are some goals or aspirtations you have for your career in software development?</p>
         <p>Some of my short-term goals are to deploy my passion project Recipe Roots. My dream is to pursue freelance web development full time and travel the world. </p>
         <p>What motivates you to continue to learn and grow as a software engineer?</p>
