718cd8ef5e99e05fff67473769a0c7b8c0555bdc
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-07-31 11:24:15 -0600

    removed footer, made navbar responsive to mobile

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,30 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faCode, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { useLocation, useHistory } from 'react-router-dom';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
-  const [showFooter, setShowFooter] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const location = useLocation();
   const history = useHistory();
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > window.innerHeight * 0.5) {
-        setShowFooter(true);
-      } else {
-        setShowFooter(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   const isSpecialPage = location.pathname !== '/';
 
@@ -47,53 +29,91 @@ const Navbar = () => {
         console.error(`Element with id ${hash} not found.`);
       }
     }
+    setIsMenuOpen(false);
   };
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setIsMenuOpen(prevState => !prevState);
   };
 
   return (
     <>
       <nav className="fixed inset-0 pointer-events-none text-xl">
         <div className="pointer-events-auto absolute top-4 left-4">
-          <a id="homeIcon" href="/">Sam<span className="inline-block hover:rotate-6 hover:-translate-y-1 transition duration-300 ease-in-out"> Cab</span></a>
+          <a id="homeIcon" href="/" onClick={() => setIsMenuOpen(false)} className="hover:text-pink-100 transition duration-300 ease-in-out">
+            Sam<span className="inline-block hover:rotate-6 hover:-translate-y-1 transition duration-300 ease-in-out"> Cab</span>
+          </a>
         </div>
         <div className="pointer-events-auto absolute top-4 right-4 md:hidden">
           <button onClick={toggleMenu} className="pointer-events-auto">
-            <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} size="2x" />
+            <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} size="1x" />
           </button>
         </div>
         <div className="pointer-events-auto absolute top-4 right-4 hidden md:flex space-x-4">
-          <button onClick={() => handleNavigation('projects')} className="pointer-events-auto">Work</button>
-          <button onClick={() => handleNavigation('blog')} className="pointer-events-auto">Blog</button>
-          <a href="/about">About</a>
+          <button onClick={() => handleNavigation('projects')} className="pointer-events-auto hover:text-pink-100 transition duration-300 ease-in-out">Work</button>
+          <button onClick={() => handleNavigation('blog')} className="pointer-events-auto hover:text-pink-100 transition duration-300 ease-in-out">Blog</button>
+          <a href="/about" className="hover:text-pink-100 transition duration-300 ease-in-out">About</a>
+          <a href="https://github.com/samanthacabrera" target="_blank" rel="noopener noreferrer" className="block w-full text-xl hover:text-pink-100 transition duration-300 ease-in-out">GitHub</a>
         </div>
       </nav>
 
       {isMenuOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center md:hidden">
-          <div className="bg-white rounded-lg shadow-lg p-8 space-y-4 text-center">
-            <button onClick={() => handleNavigation('projects')} className="block w-full text-xl">Work</button>
-            <button onClick={() => handleNavigation('blog')} className="block w-full text-xl">Blog</button>
-            <a href="/about" className="block w-full text-xl">About</a>
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center md:hidden z-50">
+          <div className="relative bg-pink-200 bg-opacity-50 rounded-lg shadow-lg p-12 space-y-4 text-center">
+            <button onClick={toggleMenu} className="absolute top-4 right-4 text-2xl text-white ">
+              <FontAwesomeIcon icon={faTimes} />
+            </button>
+            <div className="mt-10 space-y-2">
+              <button 
+                onClick={() => handleNavigation('projects')} 
+                className="block w-full text-xl text-white hover:text-pink-100 rounded-lg p-2 transition duration-300 ease-in-out"
+              >
+                Work
+              </button>
+              <button 
+                onClick={() => handleNavigation('blog')} 
+                className="block w-full text-xl text-white hover:text-pink-100 rounded-lg p-2 transition duration-300 ease-in-out"
+              >
+                Blog
+              </button>
+              <a 
+                href="/about" 
+                className="block w-full text-xl text-white hover:text-pink-100 rounded-lg p-2 transition duration-300 ease-in-out"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                About
+              </a>
+              <a 
+                href="https://linkedin.com/in/samcabreraa" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="block w-full text-xl text-white hover:text-pink-100 rounded-lg p-2 transition duration-300 ease-in-out"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                LinkedIn
+              </a>
+              <a 
+                href="https://github.com/samanthacabrera" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="block w-full text-xl text-white hover:text-pink-100 rounded-lg p-2 transition duration-300 ease-in-out"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                GitHub
+              </a>
+              <a 
+                href="https://leetcode.com/u/samcab/" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="block w-full text-xl text-white hover:text-pink-100 rounded-lg p-2 transition duration-300 ease-in-out"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                LeetCode
+              </a>
+            </div>
           </div>
         </div>
       )}
-
-      <footer className={`fixed max-w-fit bottom-4 rounded-full backdrop-blur-md inset-x-1/2 transform -translate-x-1/2 transition duration-500 ease-in-out ${showFooter ? 'opacity-100' : 'opacity-0 hidden'}`}>
-        <div className="flex justify-center items-center space-x-8 py-2 px-4">
-          <a href="https://linkedin.com/in/samcabreraa" target="_blank" rel="noopener noreferrer" className="icon-link hover:-translate-y-1 transition duration-300 ease-in-out">
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
-          </a>
-          <a href="https://github.com/samanthacabrera" target="_blank" rel="noopener noreferrer" className="icon-link hover:-translate-y-1 transition duration-300 ease-in-out">
-            <FontAwesomeIcon icon={faGithub} size="2x" />
-          </a>
-          <a href="https://leetcode.com/u/samcab/" target="_blank" rel="noopener noreferrer" className="icon-link hover:-translate-y-1 transition duration-300 ease-in-out">
-            <FontAwesomeIcon icon={faCode} size="2x" /> 
-          </a>
-        </div>
-      </footer>
     </>
   );
 };
