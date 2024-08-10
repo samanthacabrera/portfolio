447280bb292bc6a05fbbe768a8a8a703c1cfa2fa
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-08-10 17:02:25 -0600

    reverted progress bar back to original design

--- src/Progress.jsx
+++ src/Progress.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 
 function ProgressBar() {
     const [progress, setProgress] = useState(0);
-    const [isMdOrLarger, setIsMdOrLarger] = useState(window.innerWidth >= 768);
 
     useEffect(() => {
         const calculateProgress = () => {
@@ -13,41 +12,13 @@ function ProgressBar() {
             setProgress(calculatedProgress);
         };
 
-        const handleResize = () => {
-            setIsMdOrLarger(window.innerWidth >= 768);
-        };
-
-        window.addEventListener("scroll", calculateProgress);
-        window.addEventListener("resize", handleResize);
-
-        return () => {
-            window.removeEventListener("scroll", calculateProgress);
-            window.removeEventListener("resize", handleResize);
-        };
+        window.addEventListener('scroll', calculateProgress);
+        return () => window.removeEventListener('scroll', calculateProgress);
     }, []);
 
     return (
-        <div className={`progress-wrapper fixed z-50 ${isMdOrLarger ? 'inset-0' : 'top-0 left-0 w-full'}`}>
-            <div
-                className={`bar h-1 ${isMdOrLarger ? 'absolute top-0 left-0 w-full' : ''}`}
-                style={isMdOrLarger ? { width: `${progress}%` } : { width: `${progress}%` }}
-            ></div>
-            {isMdOrLarger && (
-                <>
-                    <div
-                        className="bar w-1 absolute top-0 right-0 h-full"
-                        style={{ height: `${progress}%` }}
-                    ></div>
-                    <div
-                        className="bar h-1 absolute bottom-0 left-0 w-full"
-                        style={{ width: `${progress}%` }}
-                    ></div>
-                    <div
-                        className="bar w-1 absolute top-0 left-0 h-full"
-                        style={{ height: `${progress}%` }}
-                    ></div>
-                </>
-            )}
+        <div className="progress-wrapper fixed top-0 left-0 w-full z-50">
+            <div className="bar h-1 progress" style={{ width: `${progress}%` }}></div>
         </div>
     );
 }
