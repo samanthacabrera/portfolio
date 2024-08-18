f2952d7e35fef7c00d9f6397e032ef63374d29d3
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-08-18 17:14:09 -0600

    updated intro

--- src/pages/homepage/Hero.jsx
+++ src/pages/homepage/Hero.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
+
 function Hero() {
   return (
     <div className="flex flex-col justify-center w-screen h-screen space-y-1 overflow-wrap">
+  
       <p className="text-2xl sm:text-4xl pl-1">From <span className="hover:text-slate-100 hover:text-opacity-50 duration-500">barista</span> to</p>
       <p className="text-7xl sm:text-9xl">
-        Full-Stack
+        Full-Stack  
         <span className="block hover:text-slate-100 hover:text-opacity-50 duration-500">
           Software Engineer
         </span>
       </p>
+       
       <p className="text-base sm:text-xl animate-pulseUpDown">
         scroll to read about my journey &#8595;&#8595;&#8595;&#8595;&#8595;
       </p>
@@ -18,3 +21,5 @@ function Hero() {
 }
 
 export default Hero;
+
+
