8496452d967b80de3301b7a72332a376179f70bd
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-08-30 12:13:14 -0600

    centered hero

--- src/pages/homepage/Hero.jsx
+++ src/pages/homepage/Hero.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="flex flex-col justify-center items-center md:items-start w-full h-screen space-y-1 text-center sm:text-left">
+    <div className="flex flex-col justify-center items-center  w-full h-screen space-y-4 text-center sm:text-left">
       <p className="text-2xl sm:text-4xl pl-2">
         From <span className="hover:text-slate-100 hover:text-opacity-50 duration-500">barista</span> to
       </p>
-      <p className="text-7xl sm:text-9xl">
+      <p className="text-7xl text-center sm:text-9xl">
         Full-Stack  
         <span className="block hover:text-slate-100 hover:text-opacity-50 duration-500">
           Software Engineer
         </span>
       </p>
-      <p className="text-base sm:text-xl animate-pulseUpDown">
+      <p className="text-base pt-4 sm:text-xl animate-pulseUpDown">
         scroll to read about my journey &#8595;&#8595;&#8595;&#8595;&#8595;
       </p>
     </div>
