57d265e2cff25d74baae3cbb371c49e6df7b6446
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-11-01 12:25:07 -0600

    added transition delays to Hero:

--- src/pages/homepage/Hero.jsx
+++ src/pages/homepage/Hero.jsx
@@ -5,14 +5,14 @@ function Hero() {
 
   return (
     <div className="h-screen flex">
-      <div className="flex flex-col justify-center space-y-4 text-left w-full lg:w-1/2 p-8">
+      <div className="group flex flex-col justify-center space-y-4 text-left w-full lg:w-1/2 p-8">
         <h1 className="text-4xl lg:text-6xl tracking-wider mb-2">
           Hello world!<br /> 
-          I'm <span className="text-slate-100 text-opacity-50">Sam</span> :)
+          I'm <span className="group-hover:text-slate-100 group-hover:text-opacity-50 transition duration-500">Sam</span> :)
         </h1>
-        <p className="text-lg tracking-tight max-w-lg group">
+        <p className="text-lg tracking-tight max-w-lg">
           I'm a full-stack software engineer based in Denver, specializing in web development and UX/UI design. 
-          I'm <span className="group-hover:text-slate-100 group-hover:text-opacity-50 transition duration-500">passionate</span> about creating digital solutions that seamlessly combine <span className="group-hover:text-slate-100 group-hover:text-opacity-50 transition duration-500">creativity</span> and <span className="group-hover:text-slate-100 group-hover:text-opacity-50 transition duration-500">functionality</span>. 
+          I'm <span className="group-hover:text-slate-100 group-hover:text-opacity-50 transition duration-500 delay-200">passionate</span> about creating digital solutions that seamlessly combine <span className="group-hover:text-slate-100 group-hover:text-opacity-50 transition duration-500 delay-500">creativity</span> and <span className="group-hover:text-slate-100 group-hover:text-opacity-50 transition duration-500 delay-700">functionality</span>. 
           Through this fusion, I find fulfillment and purpose in my work.
         </p>
 
