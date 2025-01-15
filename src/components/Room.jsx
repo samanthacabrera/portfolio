acf77b62758340d60cf9c1d3c036b7021df8f403
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2025-01-15 13:53:47 -0700

    basic outline for keyboard, add stand to frame

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -64,6 +64,12 @@ const Room = () => {
                     <boxGeometry args={[0.45, 0.6, 0.01]} />
                     <meshStandardMaterial map={frameTexture} />
                 </mesh>
+
+                {/* Stand */}
+                <mesh position={[0, 0.25, -0.1]} rotation={[Math.PI / 8, 0, 0]}>
+                    <boxGeometry args={[0.1, 0.5, 0.02]} />
+                    <meshStandardMaterial color="#FFFFE0" />
+                </mesh>
             </mesh>
 
             {/* Laptop */}
@@ -71,12 +77,37 @@ const Room = () => {
                 onClick={() => navigate("/work")}
                 position={[-0.3, 0.05, 0]}
                 geometries={[
-                    <boxGeometry key="laptop" args={[0.8, 0.05, 0.5]} />,
-                    <meshStandardMaterial key="laptopMaterial" color="#C0C0C0" />,
-                    <mesh key="screen" position={[0, 0.2, -0.35]} rotation={[Math.PI / 3, 0, 0]}>
+                    // Base
+                    <boxGeometry key="base" args={[0.8, 0.05, 0.5]} />,
+                    <meshStandardMaterial key="baseMaterial" color="#C0C0C0" />,
+                    // Top 
+                    <mesh key="top" position={[0, 0.2, -0.35]} rotation={[Math.PI / 3, 0, 0]}>
                         <boxGeometry args={[0.8, 0.02, 0.5]} />
                         <meshStandardMaterial color="#C0C0C0" />
                     </mesh>,
+                    // Screen
+                    <mesh key="screen" position={[0, 0.2, -0.34]} rotation={[Math.PI / 3, 0, 0]}>
+                        <boxGeometry args={[0.75, 0.02, 0.45]} />
+                        <meshStandardMaterial color="#000" />
+                    </mesh>,
+                    // Keybord
+                    ...Array.from({ length: 4 }, (_, row) =>
+                        Array.from({ length: 10 }, (_, col) => (
+                            <mesh
+                                key={`key-${row}-${col}`}
+                                position={[-0.31 + col * 0.07, 0.03, -0.15 + row * 0.07]}
+                            >
+                                <boxGeometry args={[0.06, 0.02, 0.06]} />
+                                <meshStandardMaterial color="#333" />
+                            </mesh>
+                        ))
+                    ),
+                    // Mouse
+                    <mesh key="mouse" position={[0, 0.03, 0.175]}>
+                        <boxGeometry args={[0.3, 0.01, 0.12]} />
+                        <meshStandardMaterial color="#333" />
+                    </mesh>,
+
                 ]}
             />
             {/* Mug */}
