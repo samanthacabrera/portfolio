f826928d312e5dab88b1304b404bd5fae32e1618
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2025-02-02 11:56:39 -0700

    minor fix to hero wording

--- src/pages/homepage/Hero.jsx
+++ src/pages/homepage/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
       <p className="absolute top-20 text-sm md:text-2xl max-w-sm md:max-w-3xl">
         Hello world, my name is Sam and I am a freelance developer specializing in 3D experiences.
       </p>
-      <p className="absolute top-40 opacity-70 text-sm md:text-lg max-w-sm md:max-w-3xl"> To interact with the 3D scene, you can click on the objects or hold and rotate to explore the scene.</p>
+      <p className="absolute top-40 opacity-70 text-sm md:text-lg max-w-sm md:max-w-3xl"> To interact with the 3D scene, you can click on the objects or hold and rotate to explore.</p>
     </>
   );
 }
