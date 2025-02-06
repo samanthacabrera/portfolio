6df0426f6ab879cc07be1b22171cbb87a2fd0867
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2025-02-06 16:49:56 -0700

    update vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { Crypto } from 'node-webcrypto-ossl';
+
+globalThis.crypto = new Crypto();
 
 export default defineConfig({
     plugins: [react()],
