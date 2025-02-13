6bef7006718736975c55eb8332dd781988068f57
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2025-02-13 09:50:07 -0700

    removed webcrypto dep

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { Crypto } from 'node-webcrypto-ossl';
 
-globalThis.crypto = new Crypto();
 
 export default defineConfig({
     plugins: [react()],
