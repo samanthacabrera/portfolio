d9334169f135010804973f4851d9529fe95f60a7
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2025-03-06 11:59:44 -0700

    add sage yoga mat to Yoga Scene

--- src/components/YogaMat.jsx
+++ src/components/YogaMat.jsx
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import * as THREE from "three";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, useGLTF} from "@react-three/drei";
+import Modal from "./Modal";
+
+
+
+const ClickableMesh = ({ onClick, geometries, position, rotation }) => {
+    return (
+        <mesh
+            position={position}
+            rotation={rotation}
+            onClick={onClick}
+        >
+            {geometries.map((geometry, index) => (
+                <React.Fragment key={index}>{geometry}</React.Fragment>
+            ))}
+        </mesh>
+    );
+};
 
 const YogaMat = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalContent, setModalContent] = useState(null);
+
+
+    const handleModalOpen = (content) => {
+        setModalContent(content);
+        setIsModalOpen(true);
+    };
+
+    const handleModalClose = () => {
+        setIsModalOpen(false);
+        setModalContent(null);
+    };
+
     return (
-        <div>
-            yogi side
-        </div>
+        <>
+        <Canvas
+            style={{ height: "80vh", width: "100vw" , userSelect: "none" }}
+            camera={{ position: [0, 1, 4],  rotation: [0, Math.PI / 6, 0] }}
+        >
+            <OrbitControls enableZoom={false} enablePan enableRotate />
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[5, 5, 5]} intensity={1} />
+
+            {/* Mat */}
+            <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+                <planeGeometry args={[2, 5]} />
+                <meshStandardMaterial color="#A8B89F"/>
+            </mesh>
+
+        </Canvas>
+        <Modal isOpen={isModalOpen} onClose={handleModalClose} content={modalContent} />
+        </>
     );
 };
 
--- src/pages/homepage/Hero.jsx
+++ src/pages/homepage/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <>
       <p className="absolute top-20 text-justify tracking-wide text-sm md:text-xl max-w-sm md:max-w-4xl">
-        Hello world, my name is Sam. I am equal parts web developer and yogi. I spend my days maintaining and improving <a href="https://learnyoga.online/" rel='noreferrer' target='_blank' className="italic">LearnYoga.Online</a>, and my nights building <a href="https://yarnsara-samanthacabreras-projects.vercel.app/" rel='noreferrer' target='_blank' className="italic">Yarnsara</a>. To learn more about what me & what I do interact with the scenes below. 
+        Hello world, my name is Sam. I am equal parts web developer and yogi. I spend my days maintaining and improving <span className="italic">Learn Yoga Online</span>, and my nights building <span className="italic">Yarnsara</span>. To learn more about what me & what I do interact with the scenes below. 
       </p>
       <p className="absolute bottom-20 opacity-70 text-sm md:text-lg tracking-wide max-w-sm md:max-w-4xl">
         To interact with the 3D scene, you can click on the objects or hold and rotate to explore.
