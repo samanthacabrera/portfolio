da8871739de59a0f9ae69f0a6f1d896dc0178130
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2025-03-24 12:01:52 -0600

    add group hover effect

--- src/pages/homepage/Hero.jsx
+++ src/pages/homepage/Hero.jsx
@@ -4,9 +4,9 @@ function Hero({ is3DView }) {
 
   return (
     <>
-      <p className={`${is3DView ? "absolute top-20" : "relative"} text-justify tracking-wide text-sm md:text-lg md:max-w-2xl mx-8 md:mx-auto`}>
-        <span className={`${is3DView ? "inline" : "block my-6"}`}>Hello world, my name is Sam. </span>
-        I am equal parts web developer and yogi. I spend my days maintaining and improving <a href="https://learnyoga.online/" target="_blank" rel="noopener noreferrer" className="italic font-semibold opacity-90 hover:opacity-100 transition duration-200">Learn Yoga Online</a>, and my nights building <a href="https://yarnsara-samanthacabreras-projects.vercel.app/" target="_blank" rel="noopener noreferrer" className="italic font-semibold opacity-90 hover:opacity-100 transition duration-200">Yarnsara</a>.
+      <p className={`${is3DView ? "absolute top-20" : "group relative"} text-justify tracking-wide text-sm md:text-lg md:max-w-2xl mx-8 md:mx-auto`}>
+        <span className={`${is3DView ? "inline" : "block my-6"}`}>Hello world, my name is <span className="text-lime-600 md:text-inherit group-hover:text-lime-600 transition duration-500 delay-100">Sam</span>. </span>
+        I am equal parts <span className="text-yellow-500 md:text-inherit group-hover:text-yellow-500 transition duration-500 delay-300">web developer</span> and <span className="text-cyan-600 md:text-inherit group-hover:text-cyan-600 transition duration-500 delay-500">yogi</span>. I spend my days maintaining and improving <a href="https://learnyoga.online/" target="_blank" rel="noopener noreferrer" className="italic opacity-90 hover:opacity-100 text-orange-500 md:text-inherit group-hover:text-orange-500 transition duration-500 delay-700">Learn Yoga Online</a>, and my nights building <a href="https://yarnsara-samanthacabreras-projects.vercel.app/" target="_blank" rel="noopener noreferrer" className="italic opacity-90 hover:opacity-100 text-pink-600 md:text-inherit group-hover:text-pink-600 transition duration-500 delay-700">Yarnsara</a>.
         <span className={`${is3DView ? "visible" : "invisible"}`}> To learn more about what me & what I do interact with the scenes below.</span>
       </p>
 
