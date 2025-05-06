ea439d386b1fe80675a4e3cad5a37c214758ffef
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2025-05-06 17:17:16 -0600

    add hover effect to Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div>
-            If you would like to work together, you can reach me at samantha.n.cabrera@gmail.com
+        <div className="group">
+            If you would like to work together, you can reach me at <span className="text-pink-600 md:text-inherit group-hover:text-pink-600 transition duration-500 delay-300">samantha.n.cabrera@gmail.com</span>.
         </div>
     )
 }
